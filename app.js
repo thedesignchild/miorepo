a687dc248b3c459392e863ed67452884bfc16beb
unknown <colord-team@gmail.com>
2020-05-09 12:36:00 +0530

    open calendar instead

--- app.js
+++ app.js
@@ -507,7 +507,7 @@ app.action('nudge_people', async({ ack, body, context }) => {
 app.action('schedule_hangout', async({ ack, body, context }) => {
     // Acknowledge action request
     console.log("does not open");
-    open('https://calendar.google.com/calendar');
+    await open('https://calendar.google.com/calendar');
 });
 
 
