1a6e702b8cfcbed6c786440552f1e15f18e2b9de
unknown <colord-team@gmail.com>
2020-05-20 20:31:13 +0530

    test time

--- app.js
+++ app.js
@@ -1271,7 +1271,7 @@ app.message(async({ message, body, context }) => {
                 await app.client.chat.postMessage({
                     token: context.botToken,
                     channel: '#richard-box_2',
-                    text: `*<@${message.user}> has asked for* \n` + muteTxt
+                    text: `*<@${message.user}> has asked for* \n` + txt
                 });
 
                 // identify = await app.client.users.info({
