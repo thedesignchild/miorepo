93f70f832dfc232463ab7ebfce4338820ce87f7e
unknown <colord-team@gmail.com>
2020-11-15 17:56:55 +0530

    Test

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // REQUIREMENTS
 const { App } = require('@slack/bolt');
-var openURL = require('open');
 
 // TOKENS
 const app = new App({
@@ -505,15 +504,8 @@ app.action('nudge_people', async({ ack, body, context }) => {
 });
 
 app.action('schedule_hangout', (async({ ack }) => {
-    // Opens the image in the default image viewer and waits for the opened app to quit.
-    await ack();
-    try {
-        openURL('http://en.wikipedia.org');
-        console.log('The image viewer opened');
-    } catch (err) {
-        console.log('The image viewer app quit');
-    }
 
+    await ack();
 }));
 
 
